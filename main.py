2f4216c57f6fafc639de98f658be50d382474500
alfiesiddo <alfiesiddons@gmail.com>
2025-12-03 14:54:10 +0000

    between ball and player working

--- main.py
+++ main.py
@@ -17,24 +17,34 @@ player_x = (drawer.width /2) - (player_width + 25)
 player_y = 0
 ball_x = drawer.width / 4
 ball_y = 0
-ball_starting_angle = random.randint(-1,1)
 
-if ball_starting_angle == 0:
-        ball_starting_angle = -1
+ballDirX = 0.1
+ballDirY = random.randint(-1,1)
+if ballDirY == 0:
+        ballDirY = -1
 
 def drawBall():
     global ball_x
     global ball_y
-    global ball_starting_angle
+    global ballDirY
+    global ballDirX
     drawer.Circle(ball_x, ball_y, ball_size, "white")
-    ball_x = ball_x + 1
-    ball_y = ball_y + ball_starting_angle
+    ball_x = ball_x + ballDirX
+    ball_y = ball_y + ballDirY
     
-
-
 def drawPlayer():
     drawer.Rect(player_x, player_y, player_height, player_width, "purple")
 
+
+def ballTouchPlayer():
+     global ball_x
+     global ball_y
+     global player_y
+     global player_x
+     global ballDirX
+     if ball_x >= player_x and (ball_y <= player_y) and ball_y >= player_y - player_height:
+        ballDirX *= -1
+          
 def move_left():
     global player_y
     player_y -= 30
@@ -52,3 +62,4 @@ while True:
     drawer.clearScreen()
     drawBall()
     drawPlayer()
+    ballTouchPlayer()
